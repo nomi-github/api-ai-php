8fa1ba8fed430b7f66445fb768320d8905908093
nomi-github <nomindalain@gmail.com>
2017-05-17 13:48:52 +0800

    Add files via upload

---
+++ sms_manual.php
@@ -0,0 +1,87 @@
+<?
+	include("xmlrpc.inc.php");
+	
+	header('Content-Type: application/json');
+	ob_start();
+	
+	$json = file_get_contents('php://input'); 
+	$request = json_decode($json, true);
+	
+	$action = $request["result"]["action"];
+	
+	// get parameters from request
+	$parameters = $request["result"]["parameters"];
+	$datePeriod = $parameters["datePeriod"]
+	$date = $parameters["date"]
+	$currency = $parameters["currencyType"]
+	$bank = $parameters["bank"]
+
+	// prepare parameters
+	if (is_null($bank)) {
+		$bank = ['1';'2'];
+	}
+	if (is_null($date)) {
+		$date = date("Y/m/d");
+	}
+	if (!is_null($datePeriod)) {
+		$dateFrom = substr($datePeriod, 0, 10);
+		$dateTo = substr($datePeriod, 9, 10);
+	}
+	if (is_null($dateFrom)) {
+		$dateFrom = $date;
+		$dateTo = $date;
+	}
+	
+	if (is_null($currencyType)) {
+		$currencyType = "ALL";
+	}
+	
+	if ($action == 'tdbCurrencyConverter') {
+		$output = getResult($bank, $date, $datePeriod, $currencyType);
+	}
+
+	ob_end_clean();
+	echo json_encode($output);
+	
+	
+	function getResult($bank, $date, $datePeriod, $currencyType) {
+		$vArrData=array();
+		$vArrData[]=new xmlrpcval(array(new xmlrpcval("TDB"), 
+										new xmlrpcval(md5("SERVICE")), 
+										new xmlrpcval($bank), 
+										new xmlrpcval($dateFrom),
+										new xmlrpcval($dateTo),
+										new xmlrpcval($currencyType)
+										),"struct");
+
+		$f=new xmlrpcmsg("rateHist", array(new xmlrpcval($vArrData,"array")));
+		
+		$c=new xmlrpc_client("/server.php", "172.25.0.8", 80);
+		$c->setDebug(0);
+		$r=$c->send($f);
+		$v=$r->value();
+		$speech = '';
+		if(!$r->faultCode()) 
+		{
+			$str=htmlspecialchars($v->scalarval());
+			$str=explode("\n",$str);
+			
+			foreach($str as $k=>$v)
+			{
+				$tmp=explode(";",$v);
+				print_r(json_encode($tmp));
+				$speech += json_encode($tmp);
+			}
+			return array(
+				"source": "apiai-nomi-test-currency-converter-webhook-sample"
+				"speech" => $speech,
+				"displayText" => $speech,
+				"contextOut" => array()
+				);
+		}
+		else {
+			//echo htmlspecialchars($r->faultString());
+			return "no result found"; 
+		}
+	}
+?>
